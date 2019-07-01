ea662a64f21eea74233dbb8576e24357eb3a0bae
Cerrato <48221970+Cerrat0@users.noreply.github.com>
2019-07-01 11:53:33 +0200

    Delete StringDsl.scala

--- scala-project/src/main/scala/com/fhuertas/icemd/bigdata2019/dsl/StringDsl.scala
+++
@@ -1,8 +0,0 @@
-package com.fhuertas.icemd.bigdata2019.dsl
-
-object StringDsl {
-  val wordSplitter: Seq[String] = Seq(" ", ",", ".", "\n", "\t")
-
-  def wordCount(text: String): Int =
-    text.replaceAll(s"[${wordSplitter.mkString}]+", " ").trim.split(" ").length
-}
